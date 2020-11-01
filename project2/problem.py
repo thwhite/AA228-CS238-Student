98a75ac7f7ec7fb98ef4f330b8a4f0f78bcc2c4c
thwhite <thomaswhite4@gmail.com>
2020-10-31 21:16:55 -0700

    created a class for the problem

---
+++ project2/problem.py
@@ -0,0 +1,10 @@
+class MaxLiklihoodMDP:
+
+    def __init__(self):
+        self.state = []
+        self.action_space = []
+        self.transition_count = []
+        self.reward_sum = []
+        self.discount = 0.9
+        self.value_funct = []
+        
